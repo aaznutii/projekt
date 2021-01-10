0f96ddc7d6502cea1dc59cd0d3e0695351529105
Aaznutii <aaznutii@mail.ru>
2021-01-10 22:45:03 +0400

    Создание gitignore

---
+++ main.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt6 import QtWidgets
+import firstForm
+import os
+
+
+class ExampleApp(QtWidgets.QMainWindow, firstForm.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.pushButton.clicked.connect(self.browse_folder)  # Выполнить функцию browse_folder при нажатии кнопки
+
+    def browse_folder(self):
+        self.listWidget.clear()  # На случай, если в списке уже есть элементы
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+        # открыть диалог выбора директории и установить значение переменной
+        # равной пути к выбранной директории
+
+        if directory:  # не продолжать выполнение, если пользователь не выбрал директорию
+            for file_name in os.listdir(directory):  # для каждого файла в директории
+                self.listWidget.addItem(file_name)   # добавить файл в listWidget
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec()  # и запускаем приложение
+
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
